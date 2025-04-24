60157171f8eda85f58da50a11d087305bbc90faa
poorlilgoblin <dagmara2003@gmail.com>
2025-04-24 23:02:12 +0100

    Create login.js

---
+++ pages/login.js
@@ -0,0 +1,6 @@
+// pages/login.js
+export default function LoginPage() {
+  // _app.js will intercept this route and render the Auth component,
+  // so we can just render nothing here.
+  return null;
+}
